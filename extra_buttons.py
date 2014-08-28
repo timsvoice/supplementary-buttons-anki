bb4fb43294439bf57025638741baecc6c52b9dfa
Stefan <srvandenakker@gmail.com>
2014-08-28 20:30:17 +0200

    first commit

---
+++ extra_buttons.py
@@ -0,0 +1,177 @@
+# -*- coding: utf-8 -*-
+# Written by Stefan van den Akker in 2014 <srvandenakker@gmail.com>
+#
+# TO DO:
+# - prevent QActions from hiding after they are clicked
+
+import os
+
+from anki.utils import json
+from aqt import editor, mw
+from anki.hooks import wrap
+from PyQt4 import QtGui, QtCore
+
+
+# Preferences
+##################################################
+
+def addons_folder():
+    return mw.pm.addonFolder()
+
+addon_path = os.path.join(addons_folder(), ".extra_buttons_prefs")
+
+default_conf = {"class_name": "",
+                "Show <code> button": True,
+                "Show unordered list button": True,
+                "Show ordered list button": True,
+                "Show strikethrough button": True,
+                "Show code block button": True,
+                "Show horizontal rule button": True,
+                "Show indent button": True}
+
+try:
+    with open(addon_path, "r") as f:
+        prefs = json.load(f)
+except IOError:
+    with open(addon_path, "w") as f:
+        prefs = default_conf
+        json.dump(prefs, f)
+
+def save_prefs():
+    with open(addon_path, "w") as f:
+        json.dump(prefs, f)
+
+def get_class_name(self):
+    """Sets the CSS styling for the <code> and <pre> tags"""
+
+    current_text = prefs.get("class_name", "")
+
+    text, ok = QtGui.QInputDialog.getText(mw, "Set class for <code> and <pre>",
+        "Enter a class name for your custom CSS &lt;code&gt; and &lt;pre&gt; style",
+        QtGui.QLineEdit.Normal, current_text)
+
+    if ok:
+        prefs["class_name"] = text
+        save_prefs()
+
+
+# Menu
+##################################################
+
+class ExtraButtons_Options(QtGui.QMenu):
+    "Displays the various options in the main menu."
+    def __init__(self, mw):
+        super(ExtraButtons_Options, self).__init__()
+        self.mw = mw
+
+    def button_switch(self):
+        "Puts a button either on or off. Reverses current state."
+        source = self.sender()
+        name = source.text()
+        current_state = prefs[name]
+        prefs[name] = not current_state
+        save_prefs()
+
+    def create_action(self, name, mw, checkable=True):
+        action = QtGui.QAction(name, mw)
+        if checkable:
+            action.setCheckable(True)
+            if prefs[name]:
+                action.setChecked(True)
+        action.toggled.connect(self.button_switch)
+        return action
+
+    def setup_extra_buttons_options(self):
+
+        sub_menu = mw.form.menuTools.addMenu("&Extra buttons add-on (options)")
+
+        # go through all the keys in the preferences and make QActions for them
+        for option in sorted(prefs.keys()):
+            if option in ["class_name"]:
+                continue
+            else:
+                action = self.create_action(option, mw)
+                sub_menu.addAction(action)
+
+        custom_css = QtGui.QAction("&Alter <code> CSS...", mw)
+        custom_css.triggered.connect(get_class_name)
+        
+        sub_menu.addAction(custom_css)
+
+
+# Buttons
+##################################################
+
+def mySetupButtons(self):
+
+    if prefs["Show <code> button"]:
+        self._addButton("text_code", self.toggleCode, _("Ctrl+,"),
+            _("Code format text (Ctrl+,)"), check=False, text=u"<>")
+
+    if prefs["Show unordered list button"]:
+        self._addButton("unordered_list", self.toggleUnorderedList, _("Ctrl+["),
+            _("Create unordered list (Ctrl+[)"), check=False, text=u"*")
+
+    if prefs["Show ordered list button"]:
+        self._addButton("ordered_list", self.toggleOrderedList, _("Ctrl+]"),
+            _("Create ordered list (Ctrl+])"), check=False, text=u"1.")
+
+    if prefs["Show strikethrough button"]:
+        self._addButton("text_strikethrough", self.toggleStrikeThrough,
+            _("Alt+Shift+5"), _("Strikethrough text (Alt+Shift+5)"), check=True,
+            text=u"Z\u0336")
+
+    if prefs["Show code block button"]:
+        # FIX ME: think of better symbol to represent a <pre> block
+        self._addButton("text_pre", self.togglePre, _("Ctrl-."),
+            tip=_("Create a code block (Ctrl-.)"), check=False, text=u"{}")
+
+    if prefs["Show horizontal rule button"]:
+        self._addButton("hor_line", self.toggleHorizontalLine, _("Ctrl+h"),
+            tip=_("Create a horizontal line (Ctrl+H)"),
+            check=False, text=u"\u2014")
+
+    if prefs["Show indent button"]:
+        self._addButton("indent", self.toggleIndent, _("Ctrl+Shift+i"),
+            _("Indent text or list (Ctrl+Shift+I)"), check=False, text=u">>")
+
+def toggleCode(self):
+    # if a custom name for the CSS governing <code> is given, use it
+    if prefs["class_name"]:
+        # the individable separator &#8291 was added to solve difficulties
+        # where the wrap wouldn't work right after a <div> element
+        self.web.eval("wrap('&#8291;<code class={}>', '</code>');".format(
+            prefs["class_name"]))
+    else:
+        self.web.eval("wrap('&#8291;<code>', '</code>');")
+
+def toggleUnorderedList(self):
+    self.web.eval("setFormat('insertUnorderedList')")
+
+def toggleOrderedList(self):
+    self.web.eval("setFormat('insertOrderedList')")
+
+def toggleStrikeThrough(self):
+    self.web.eval("setFormat('strikeThrough')")
+
+def togglePre(self):
+    self.web.eval("setFormat('formatBlock', 'pre')")
+
+def toggleHorizontalLine(self):
+    self.web.eval("setFormat('insertHorizontalRule')")
+
+def toggleIndent(self):
+    self.web.eval("setFormat('indent')")
+
+
+editor.Editor.toggleCode = toggleCode
+editor.Editor.toggleOrderedList = toggleOrderedList
+editor.Editor.toggleUnorderedList = toggleUnorderedList
+editor.Editor.toggleStrikeThrough = toggleStrikeThrough
+editor.Editor.togglePre = togglePre
+editor.Editor.toggleHorizontalLine = toggleHorizontalLine
+editor.Editor.toggleIndent = toggleIndent
+editor.Editor.setupButtons = wrap(editor.Editor.setupButtons, mySetupButtons)
+
+mw.ExtraButtons_Options = ExtraButtons_Options(mw)
+mw.ExtraButtons_Options.setup_extra_buttons_options()
---
+++ readme.md
@@ -0,0 +1,31 @@
+This add-on adds the following buttons to Anki:
+
+<ul>
+<li>a <b>code</b> button that will wrap selected text in a <code>&lt;code&gt;</code> element (shortcut: <b>Ctrl+,</b>). You can specify the CSS class you want to use in combination with <code>&lt;code&gt;</code>. For example, we have a CSS class named <code>c</code> defined in the <i>Styling</i> section of <i>Cards</i>:
+<br /><br />
+<code>.c {<br />
+ font-family: droid sans mono;<br />
+ background-color: #f2f2f2;<br />
+ padding-left: 5px;<br />
+ padding-right: 5px;<br />
+}</code>
+<br /><br />
+In the options we can specify the class name, so that our <code>&lt;code&gt;</code> elements will be automatically transformed to <code>&lt;code class="c"&gt;</code>.</li>
+<li>an <b>unordered list</b> button (shortcut <b>Ctrl+[</b>):
+<br /><br />
+<ul><li>One</li><li>Two</li><li>Three</li></ul>
+<br /></li>
+<li>an <b>ordered list</b> button (shortcut <b>Ctrl+]</b>):
+<br /><br />
+<ol><li>One</li><li>Two</li><li>Three</li></ol></li>
+<br />
+<li>an <b>indent</b> button (shortcut <b>Ctrl+Shift+I</b>) to indent text or lists:
+<br /><br />
+<ol><li>One</li><ul><li>Two</li><ol><li>Three</li></ol></ul></ol>
+<br />
+<li>a <b>strikethrough</b> button (shortcut <b>Alt+Shift+5</b>):
+<br /><br />
+<ul><s>strikethrough text example</s></ul>
+<br />
+<li>a <b>code block</b> button (shortcut <b>Ctrl+.</b>) that creates a <code>&lt;pre&gt;</code> block element around the selected element
+<li>a <b>horizontal rule</b> button (shortcut <b>Ctrl+H</b>) that inserts a horizontal rule after the current position of the cursor
